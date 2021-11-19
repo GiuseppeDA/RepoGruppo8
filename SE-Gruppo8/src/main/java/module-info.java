f0c18c32141b8be515e1e7afa8ba216cab22ba02
mbreaker <mario-boccia@hotmail.it>
2021-11-19 12:35:31 +0100

    deleting folder

--- SE-Gruppo8/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.mycompany.se.gruppo8 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.se.gruppo8 to javafx.fxml;
-    exports com.mycompany.se.gruppo8;
-    requires jcomplex;
-}
