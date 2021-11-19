2a601c3b6ff690b830195b7490e57c3a5d385f69
mbreaker <mario-boccia@hotmail.it>
2021-11-19 12:13:56 +0100

    aggiunto progetto fatto con maven - no Libreria Complex!

---
+++ SE-Gruppo8/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module com.mycompany.se.gruppo8 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens com.mycompany.se.gruppo8 to javafx.fxml;
+    exports com.mycompany.se.gruppo8;
+}
