8a5237a38b2e2555ec38a76a9bbbc19ad67db5f9
SGRANATO9 <s.granato9@studenti.unisa.it>
2021-11-23 11:58:55 +0100

    emptyCalculator

---
+++ SEGruppo8/src/main/java/it/unisa/SoftwreEngineering/Gruppo8/Calculator.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.unisa.SoftwreEngineering.Gruppo8;
+import com.vm.jcomplex.Complex;
+import java.lang.UnsupportedOperationException;
+import java.util.Stack;
+/**
+ *
+ * @author 39324
+ */
+public class Calculator {
+    private Stack<Complex> memory;
+    
+    public void add(){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    public void subcract(){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    public void multiply(){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    public void divide (){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    public void sqrt(){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    public void invert (){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    public Complex getTop(){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    protected boolean checkSize(int a){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    public void insert(){
+        throw new UnsupportedOperationException("Not supported yet.");
+    } 
+}
