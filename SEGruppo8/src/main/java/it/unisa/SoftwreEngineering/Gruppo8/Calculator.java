b8541bfe4ee02a18a7bff9a270ac90c4704254b5
SGRANATO9 <s.granato9@studenti.unisa.it>
2021-11-23 12:12:04 +0100

    added example javadoc

--- SEGruppo8/src/main/java/it/unisa/SoftwreEngineering/Gruppo8/Calculator.java
+++ SEGruppo8/src/main/java/it/unisa/SoftwreEngineering/Gruppo8/Calculator.java
@@ -13,30 +13,68 @@ import java.util.Stack;
 public class Calculator {
     private Stack<Complex> memory;
     
+    /**
+     *Somma i primi due valori in memoria
+     */
     public void add(){
         throw new UnsupportedOperationException("Not supported yet.");
     }
+
+    /**
+     *
+     */
     public void subcract(){
         throw new UnsupportedOperationException("Not supported yet.");
     }
+
+    /**
+     *
+     */
     public void multiply(){
         throw new UnsupportedOperationException("Not supported yet.");
     }
+
+    /**
+     *
+     */
     public void divide (){
         throw new UnsupportedOperationException("Not supported yet.");
     }
+
+    /**
+     *
+     */
     public void sqrt(){
         throw new UnsupportedOperationException("Not supported yet.");
     }
+
+    /**
+     *
+     */
     public void invert (){
         throw new UnsupportedOperationException("Not supported yet.");
     }
+
+    /**
+     *
+     * @return l'oggetto Complex in cima alla memory
+     */
     public Complex getTop(){
         throw new UnsupportedOperationException("Not supported yet.");
     }
+
+    /**
+     *
+     * @param a è il numero minimo di oggetti che devono essere presenti nello stack
+     * @return true se il numero di oggetti in memoria non è minore di a, altrimenti false
+     */
     protected boolean checkSize(int a){
         throw new UnsupportedOperationException("Not supported yet.");
     }
+
+    /**
+     *
+     */
     public void insert(){
         throw new UnsupportedOperationException("Not supported yet.");
     } 
