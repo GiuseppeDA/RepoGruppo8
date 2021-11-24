14ff42a77c5d2f4b0ec58b3654bd9de5be4d45c2
giuseppe <g.dambrosio55@studenti.unisa.it>
2021-11-24 16:54:52 +0100

    Test popup

--- SEGruppo8/src/main/java/it/unisa/SoftwreEngineering/Gruppo8/SecondaryController.java
+++ SEGruppo8/src/main/java/it/unisa/SoftwreEngineering/Gruppo8/SecondaryController.java
@@ -2,11 +2,39 @@ package it.unisa.SoftwreEngineering.Gruppo8;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.*;
 
 public class SecondaryController {
 
+    /*
     @FXML
     private void switchToPrimary() throws IOException {
         App.setRoot("primary");
     }
+    */
+    public static void display(String message){
+        Stage window = new Stage();
+    
+        window. initModality(Modality.APPLICATION_MODAL);
+        window.setTitle("Error");
+        window.setMinWidth(250);
+        Label label = new Label();
+        label.setText(message);
+        Button closeButton = new Button ("Ok");
+        closeButton.setOnAction(e -> window.close());
+        
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(label, closeButton);
+        layout.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(layout);
+        window.setScene(scene);
+        window.showAndWait();
+    }
+    
 }
