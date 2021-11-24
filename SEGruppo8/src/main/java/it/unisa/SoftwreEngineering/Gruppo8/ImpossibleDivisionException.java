e01622bb7471b0678fb4660f417c05f2717b281b
39324 <39324@LAPTOP-1MG9BKAN.mshome.net>
2021-11-24 17:35:03 +0100

    create ImpossibleDivisionException

---
+++ SEGruppo8/src/main/java/it/unisa/SoftwreEngineering/Gruppo8/ImpossibleDivisionException.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Exception.java to edit this template
+ */
+package it.unisa.SoftwreEngineering.Gruppo8;
+
+/**
+ *
+ * @author 39324
+ */
+public class ImpossibleDivisionException extends Exception {
+
+    /**
+     * Creates a new instance of <code>ImpossibleDivisionException</code>
+     * without detail message.
+     */
+    public ImpossibleDivisionException() {
+    }
+
+    /**
+     * Constructs an instance of <code>ImpossibleDivisionException</code> with
+     * the specified detail message.
+     *
+     * @param msg the detail message.
+     */
+    public ImpossibleDivisionException(String msg) {
+        super(msg);
+    }
+}
