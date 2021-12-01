4d57ba8e3a2f69eb7df9f7f6db257b643cb977c4
39324 <39324@LAPTOP-1MG9BKAN.mshome.net>
2021-12-01 19:26:45 +0100

    Variable create

---
+++ SEGruppo8/src/main/java/it/unisa/SoftwreEngineering/Gruppo8/Variable.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.unisa.SoftwreEngineering.Gruppo8;
+
+import com.vm.jcomplex.Complex;
+
+/**
+ *
+ * @author 39324
+ */
+public class Variable {
+    String name;
+    Complex value;
+
+    public Variable(String name, Complex value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Complex getValue() {
+        return value;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setValue(Complex value) {
+        this.value = value;
+    }
+    
+}
