2cd60e8091207d275a27df893861c6a49618012b
39324 <39324@LAPTOP-1MG9BKAN.mshome.net>
2021-12-01 11:48:37 +0100

    variables class created

---
+++ SEGruppo8/src/main/java/it/unisa/SoftwreEngineering/Gruppo8/Variables.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.unisa.SoftwreEngineering.Gruppo8;
+
+import com.vm.jcomplex.Complex;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+/**
+ *
+ * @author 39324
+ */
+public class Variables {
+    private ObservableList<Complex> variablesList;
+
+    public Variables() {
+         variablesList= FXCollections.observableArrayList();
+    }
+    
+}
+
