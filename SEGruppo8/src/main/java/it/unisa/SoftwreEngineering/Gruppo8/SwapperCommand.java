9c4e60d9c48f5de51b6b9da052d9d111eaa62059
simon <simon@LAPTOP-6Q5OGHGB>
2021-12-07 12:01:11 +0100

    commad classes created

---
+++ SEGruppo8/src/main/java/it/unisa/SoftwreEngineering/Gruppo8/SwapperCommand.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.unisa.SoftwreEngineering.Gruppo8;
+
+/**
+ *
+ * @author simon
+ */
+public class SwapperCommand implements Command{
+
+    @Override
+    public void execute() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void undo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
