3b81254cb3e8e5ca461d3e641de0d9a59099db65
39324 <39324@LAPTOP-1MG9BKAN.mshome.net>
2021-12-07 11:46:58 +0100

    Command interface

---
+++ SEGruppo8/src/main/java/it/unisa/SoftwreEngineering/Gruppo8/Command.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package it.unisa.SoftwreEngineering.Gruppo8;
+
+/**
+ *
+ * @author 39324
+ */
+public interface Command {
+    public void execute ();
+    public void undo();
+    
+}
