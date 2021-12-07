d8d381e20d2a0de6b0cf234e3800334c9ec6a4ac
39324 <39324@LAPTOP-1MG9BKAN.mshome.net>
2021-12-07 13:05:24 +0100

    Functions created

---
+++ SEGruppo8/src/main/java/it/unisa/SoftwreEngineering/Gruppo8/Functions.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.unisa.SoftwreEngineering.Gruppo8;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableMap;
+
+
+/**
+ *
+ * @author 39324
+ */
+public class Functions {
+    ObservableMap<String,Function> map;
+
+    public Functions() {
+        map=FXCollections.emptyObservableMap();
+    }
+    
+}
