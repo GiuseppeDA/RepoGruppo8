6a82c3349616ffb5d798ba14727fcb6fa1e5d132
simon <simon@LAPTOP-6Q5OGHGB>
2021-12-07 15:17:42 +0100

    test tostring addercommand passed

--- SEGruppo8/src/test/java/it/unisa/SoftwreEngineering/Gruppo8/AdderCommandTest.java
+++ SEGruppo8/src/test/java/it/unisa/SoftwreEngineering/Gruppo8/AdderCommandTest.java
@@ -76,12 +76,11 @@ public class AdderCommandTest {
     @Test
     public void testToString() {
         System.out.println("toString");
-        AdderCommand instance = null;
-        String expResult = "";
+        Calculator calc = new Calculator();
+        AdderCommand instance = new AdderCommand(calc);
+        String expResult = "+";
         String result = instance.toString();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
     
 }
